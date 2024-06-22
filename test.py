81bbd8a26d332b1052c4a09c97a123fe459d7360
WaRKaZz <33zy33@gmail.com>
2024-06-22 08:57:17 +0500

    clean up Aigul's filenames

--- test.py
+++ test.py
@@ -1,39 +1,9 @@
-import flet as ft
+import os
 
-def main(page: ft.Page):
-    page.title = "Routes Example"
+path = "C:\\Intel\\PRJ\\nomerin-aitashy\\resources\\voice_aigul"
 
-    def route_change(route):
-        page.views.clear()
-        page.views.append(
-            ft.View(
-                "/",
-                [
-                    ft.AppBar(title=ft.Text("Flet app"), bgcolor=ft.colors.SURFACE_VARIANT),
-                    ft.ElevatedButton("Visit Store", on_click=lambda _: page.go("/store")),
-                ],
-            )
-        )
-        if page.route == "/store":
-            page.views.append(
-                ft.View(
-                    "/store",
-                    [
-                        ft.AppBar(title=ft.Text("Store"), bgcolor=ft.colors.SURFACE_VARIANT),
-                        ft.ElevatedButton("Go Home", on_click=lambda _: page.go("/")),
-                    ],
-                )
-            )
-        page.update()
+files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
 
-    def view_pop(view):
-        page.views.pop()
-        top_view = page.views[-1]
-        page.go(top_view.route)
-
-    page.on_route_change = route_change
-    page.on_view_pop = view_pop
-    page.go(page.route)
-
-
-ft.app(target=main, view=ft.AppView.WEB_BROWSER)
+for file in files:
+    if " " in file:
+        os.rename(path + "\\" + file, path + "\\" + file.replace(" ", ""))
