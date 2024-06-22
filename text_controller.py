d5855ed01392a8949ee26700a84fb7bc9e4e2e7a
WaRKaZz <33zy33@gmail.com>
2024-06-22 19:28:50 +0500

    created blank for Text controller

--- text_controller.py
+++ text_controller.py
@@ -1,9 +1,14 @@
-welcome_message_txt = "Hello, fellow User, this applcation will teach how to count in Kazakh language \nPress Start to start your Kazakh language practice"
-welcome_txt = "Welcome"
-guide_txt = "Guide"
-practice_txt = "Practice"
-about_txt = "About Project"
-start_txt = "Start"
-settings_txt = "Settings"
-
+import configparser
 # TODO Create proper Text Controller and refactor every text getter
+class txt_ctl:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def __class_getitem__ (self, name: str) -> str:
+        config = configparser.RawConfigParser()
+        config.read("language.properties")
+        return config.get("en_US",name)
+    
+if __name__ == "__main__":
+    print(txt_ctl['welcome_txt'])
